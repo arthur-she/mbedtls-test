c5170793bb382cf9bcb25a7efc61126d264f9835
Leonardo Sandoval <leonardo.sandoval@linaro.org>
2021-11-27 16:00:55 -0600

    Switch to Open CI Arm license server
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval@linaro.org>

--- vars/gen_jobs.groovy
+++ vars/gen_jobs.groovy
@@ -222,7 +222,7 @@ echo >&2 'Note: "clang" will run /usr/bin/clang -Wno-error=c11-extensions'
 #!/bin/sh
 set -eux
 ulimit -f 20971520
-export ARMLMD_LICENSE_FILE="7010@10.6.26.52:7010@10.6.26.53:7010@10.6.26.54:7010@10.6.26.56"
+export ARMLMD_LICENSE_FILE="27000@flexnet.trustedfirmware.org"
 export MBEDTLS_TEST_OUTCOME_FILE='${job_name}-outcome.csv'
 ${extra_setup_code}
 ./tests/scripts/all.sh --seed 4 --keep-going $component
@@ -506,7 +506,7 @@ pip install -r requirements.txt
                             sh """\
 ulimit -f 20971520
 . $WORKSPACE/mbed-venv/bin/activate
-export ARMLMD_LICENSE_FILE="7010@10.6.26.52:7010@10.6.26.53:7010@10.6.26.54:7010@10.6.26.56"
+export ARMLMD_LICENSE_FILE="27000@flexnet.trustedfirmware.org"
 mbed compile -m ${platform} -t ${compiler}
 """
                             for (int attempt = 1; attempt <= 3; attempt++) {
