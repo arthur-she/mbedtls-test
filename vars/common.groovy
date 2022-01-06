cfa4302cd3bfa94b633c701f25d52f5fac064ba4
Arthur She <arthur.she@linaro.org>
2022-01-05 22:01:57 -0800

    generate temporary directory
    Signed-off-by: Arthur She <arthur.she@linaro.org>

--- vars/common.groovy
+++ vars/common.groovy
@@ -114,7 +114,7 @@ def docker_script(platform, entrypoint, entrypoint_arguments='') {
     def docker_image = get_docker_tag(platform)
     return """\
 docker run -u \$(id -u):\$(id -g) --rm --entrypoint $entrypoint \
-    -w /var/lib/build -v `pwd`/src:/var/lib/build \
+    -w /var/lib/build -v /data/shared/`echo ${env.EXECUTOR_NUMBER}`/src:/var/lib/build \
     --cap-add SYS_PTRACE $docker_repo:$docker_image $entrypoint_arguments
 """
 }
